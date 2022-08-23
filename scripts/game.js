0494801a07ddaad26ac0890eaf8221decd42f522
SamHulme1 <samhulmephotography@gmail.com>
2022-08-23 20:07:53 +0000

    finsished the generate room function. it now assigns an image for the user to veiw by manipulating the dom, changed the class name of output image to be hero image. added some placeholder p elements in the scoreboard for where the users score will be displayed to them.

--- scripts/game.js
+++ scripts/game.js
@@ -10,35 +10,65 @@ let game = {
  */
 
 /**
- * This function generates a randomRoom and then pushes the room to the room array in game, the room generated contains a text description which is then read later on in the code. At the start of the fuction the room array in the game is cleared so that a new value can be stored.
+ * This function generates a randomRoom and then pushes the room to the room array in game, the room generated contains a text description which is then read later on in the code. At the start of the fuction the room array in the game is cleared so that a new value can be stored. 
  */
 
 function generateRoom() {
+    // reset the game room array
     game.room = [];
+    // generate a random number 
     let randomRoom = Math.floor(Math.random()*10) +1;
-    roomImage = document.getElementsByClassName("output-image");
+    //store the element with the class name of output-image in variable room image
+    let roomImage = document.getElementById("output-image");
+    // if statement to assign the generated number a room
     if (randomRoom === 1){
+        // push the room description to the room array
         game.room.push('You come accross a partial cave-in, after moving some of the rubble, you discover a small passageway "I shouldnt have had that extra large breakfast" you think to yourself as you just about squeeze through. After following the passage for several minutes you feel the walls begin to move back and open up into a small chamber inside you find a');
-        roomImage.src = "";
+        // assign the correct image to the correct room. change the innerHTML of roomImage to relect this
+        roomImage.innerHTML = ` <img class ="hero-image center" src="img/tavern.jpg" alt="the dugeon maze title image">`;
     } else if (randomRoom === 2){
+        // push the room description to the room array
         game.room.push('You approach two large ajar doors, which guard a large room. Inside the room sits a large retangular table infront of a roaring fireplace. On the table rest a number of plates a goblets with what smell like some sort of ale inside. Opposite the fire there is a larger throne sized chair made of old weponry. The room also features several small paintings and a cupboard containing strange victuals "guess ill skip lunch then" you think to yourself after smelling some of the food. After searching the room you find a ');
-        roomImage = document.getElementsByClassName("output-image").src = "img/largeroom-1.jpg";
+         // assign the correct image to the correct room. change the innerHTML of roomImage to relect this
+        roomImage.innerHTML = ` <img class ="hero-image center" src="img/largeroom-1.jpg" alt="the dugeon maze title image">`;
     } else if (randomRoom === 3){
+        // push the room description to the room array
         game.room.push('The passageway suddenly opens up into a large carved out room. Inside the room you can see an alter and what look like small wooden benches. "Maybe this used to be some kind of worshiping area" you think to yourself as you move further into the room. On the walls hang several small daggers and a number of simialrly sized painting depicting some sort of goblin god. There is a large pipe organ. several disguarded sheets with what looks like music written on them line the floor "or tourture chamber you laugh to yourself" After looking around some more you find a ');
-        roomImage = document.getElementsByClassName("output-image").src = "img/largeroom-2.jpg";
+         // assign the correct image to the correct room. change the innerHTML of roomImage to relect this
+        roomImage.innerHTML = ` <img class ="hero-image center" src="img/large-room-2.jpg" alt="the dugeon maze title image">`;
     } else if (randomRoom === 4){
+        // push the room description to the room array
         game.room.push('After following a long corridoor for sometime, you come across a small dimply lit room. Inside the room is a small round table with cards layed out ontop of it. Further back in the room are two small size jail cells with the remains of somesort of humanoid creatures inside. It looks like whoever locked these poor souls inside didnt like loosing after inspecting the room further you find a ');
+         // assign the correct image to the correct room. change the innerHTML of roomImage to relect this
+        roomImage.innerHTML = ` <img class ="hero-image center" src="img/small-room-1.jpg" alt="the dugeon maze title image">`;
     } else if (randomRoom === 5){
+        // push the room description to the room array
         game.room.push('At the end of the path you see a small closed door, you knock a few times and to your suprise ... nobody answers. "well at least I tried" you think to yourself as you try the handle. The door falls open and inside you can see large crates, barrels and other storage containers you decide to open them. Inside you find a ');
+         // assign the correct image to the correct room. change the innerHTML of roomImage to relect this
+        roomImage.innerHTML = ` <img class ="hero-image center" src="img/small-room-2.jpg" alt="the dugeon maze title image">`;
     } else if (randomRoom === 6){
+        // push the room description to the room array
         game.room.push('You enter a medium sized room, inside the room are several small sized beds. At the bottom of each bed sits a small wooden trunk. The walls are decorated with colourful dyed fabrics. Mainly green though. Afterall, it is a goblins favorite colour. There are several small hanging lanterns making sure that the room is well lit. You look through the trunks and discover a ');
+         // assign the correct image to the correct room. change the innerHTML of roomImage to relect this
+        roomImage.innerHTML = ` <img class ="hero-image center" src="img/bedroom.jpg" alt="the dugeon maze title image">`;
     } else if (randomRoom === 7){
+        // push the room description to the room array
         game.room.push('"well, well what do we have here" you say as you approach a large stone well. The well well has long since emptied but the pully system still works. Just about, you pull the bucket out and inside find a  ');
+         // assign the correct image to the correct room. change the innerHTML of roomImage to relect this
+        roomImage.innerHTML = ` <img class ="hero-image center" src="img/well.jpg" alt="the dugeon maze title image">`;
     } else if (randomRoom === 8){
+        // push the room description to the room array
+        
         game.room.push(' You come acoss a large stone locked door. You try everything but the door wont move without its proper key. Youre forced to turn back. On the plus side you find a ');
+         // assign the correct image to the correct room. change the innerHTML of roomImage to relect this
+        roomImage.innerHTML = ` <img class ="hero-image center" src="img/locked-door.jpg" alt="the dugeon maze title image">`;
     } else {
+        // push the room description to the room array
         game.room.push('You follow a long corridoor. On the walls hang metal lanterns the corridoor is make from rock, rock and somemore rock. Whilst you walk down the corridoor you find a ');
+         // assign the correct image to the correct room. change the innerHTML of roomImage to relect this
+        roomImage.innerHTML = ` <img class ="hero-image center" src="img/corridoor.jpg" alt="the dugeon maze title image">`;
     }
+     // return the generated value to the room array in the game object
     return game.room;
 }
 
